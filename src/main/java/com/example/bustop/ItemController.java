3f7bf181a9d7bf6466019d1f0a9242c0e5d18786
Taesan <xotks7524@gmail.com>
2024-01-31 19:56:45 +0900

    first commit

--- src/main/java/com/example/bustop/ItemController.java
+++ src/main/java/com/example/bustop/ItemController.java
@@ -1,2 +1,8 @@
-package com.example.bustop;public class ItemController {
+package com.example.bustop;
+
+public class ItemController {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
+
