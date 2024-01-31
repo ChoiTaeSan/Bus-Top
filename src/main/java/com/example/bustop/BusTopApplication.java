32049ddbf14c315c41f42dfb6286b2ad177953c8
Choi TaeSan <xotks7524@gmail.com>
2024-01-31 18:11:42 +0900

    first commit

---
+++ src/main/java/com/example/bustop/BusTopApplication.java
@@ -0,0 +1,13 @@
+package com.example.bustop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTopApplication.class, args);
+    }
+
+}
