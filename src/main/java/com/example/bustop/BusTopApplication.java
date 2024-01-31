4eb05875ea151e8d0c96fec13c3d1d6315e029e0
Taesan <xotks7524@gmail.com>
2024-01-31 20:33:51 +0900

    first commit

--- src/main/java/com/example/bustop/BusTopApplication.java
+++ src/main/java/com/example/bustop/BusTopApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusTopApplication {
-
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(i);
-        }
+        SpringApplication.run(BusTopApplication.class, args);
     }
 
 }
