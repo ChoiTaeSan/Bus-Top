ee9297449744ef2126c5f230b77774dcc046c977
hi2li <ckdtjs584@naver.com>
2024-02-01 19:40:59 +0900

    "first commitdoesnt have a special things"

---
+++ src/main/java/com/example/bus_top/BusTopApplication.java
@@ -0,0 +1,13 @@
+package com.example.bus_top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTopApplication.class, args);
+    }
+
+}
